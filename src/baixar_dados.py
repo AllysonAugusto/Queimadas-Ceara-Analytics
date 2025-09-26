f82e21d72c91da619b5e16c4605e9a05606129c9
AllysonAugusto <allysonfn9@hotmail.com>
2025-09-26 19:47:13 -0300

    feat: adiciona função para ler arquivos CSV extraídos

--- src/baixar_dados.py
+++ src/baixar_dados.py
@@ -3,7 +3,7 @@ import requests
 import zipfile
 import os
 
-  #Baixar dados
+#Baixar dados
 def baixar_dados_ce(ano, pasta_raiz="dados_ceara"):
   url = f"https://dataserver-coids.inpe.br/queimadas/queimadas/focos/csv/anual/EstadosBr_sat_ref/CE/focos_br_ce_ref_{ano}.zip"
   print(f"Baixando dados do ano {ano}...")
@@ -15,11 +15,13 @@ def baixar_dados_ce(ano, pasta_raiz="dados_ceara"):
     print(f"Erro ao baixar dados do ano {ano}: {e}")
     return None
 
-#Salvar zip
+  #Pasta do ano
+  pasta_ano = os.path.join(pasta_raiz, f"ano_{ano}")
+  os.makedirs(pasta_ano, exist_ok=True)
 
+  #Salvar zip
   try:
-    os.makedirs(pasta_raiz, exist_ok = True)
-    zip_path = os.path.join(pasta_raiz, f"focos_br_ce_ref_{ano}.zip")
+    zip_path = os.path.join(pasta_ano, f"focos_br_ce_ref_{ano}.zip")
     with open(zip_path, "wb") as f:
       for chunk in resposta.iter_content(chunk_size=8192):
         f.write(chunk)
@@ -28,23 +30,62 @@ def baixar_dados_ce(ano, pasta_raiz="dados_ceara"):
   except OSError as e:
     print(f"Erro ao salvar o arquivo zip: {e}")
     return None
-    
+
 #Extrair todos os arquivos do zip
   try:
     with zipfile.ZipFile(zip_path, "r") as z:
-      z.extractall(pasta_raiz)
-      print(f"Todos os arquivos extraidos em: {pasta_raiz}")
+      z.extractall(pasta_ano)
+      print(f"\nTodos os arquivos extraidos em: {pasta_ano}")
   except zipfile.BadZipFile as e:
     print("Erro: Zip corrompido")
     return None
   except Exception as e:
     print(f"Erro ao extrair ZIP:{e}")
     return None
-  
-  return pasta_raiz
+
+  return pasta_ano
 
 
+def ler_csv_extraido(pasta_raiz):
+  try:
+    arquivos = os.listdir(pasta_raiz)
+    arquivos_csv = [arquivo for arquivo in arquivos if arquivo.endswith(".csv")]
+    
+    
+    if not arquivos_csv:
+      print("Nenhum arquivo CSV encontrado na pasta extraida")
+      return None
 
-pasta = baixar_dados_ce(2023)
-if pasta:
-  print("Download e extracao concluidos com sucesso")
+    dfs = []
+
+    for arquivo in arquivos_csv:
+      caminho_csv = os.path.join(pasta_raiz, arquivo)
+      print(f"\nLendo arquivo CSV: {caminho_csv}")
+
+      df = pd.read_csv(caminho_csv, sep=",", encoding="utf-8", low_memory=False)
+
+      dfs.append(df)
+
+    return dfs
+
+  except Exception as e:
+    print(f"Erro ao ler o arquivo CSV: {e}")
+    return None
+
+if __name__ == "__main__":
+  ano = input("Digite o ano desejado (ex: 2023): ")
+
+  try:
+    ano = int(ano)
+  except ValueError:
+    print("Ano inválido. Certifique-se de digitar um número inteiro.")
+    exit()
+
+  pasta = baixar_dados_ce(ano)
+  if pasta:
+    print("\nDownload e extração concluidos.")
+
+    dfs = ler_csv_extraido(pasta, ano)
+    if dfs:
+      print("-=-=-=-=" * 30)
+      print(dfs[0].head())
